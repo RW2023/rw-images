b244b4322120809fcf6eeecb754af7c6e0425216
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-08 07:29:29 -0500

    install document.tsx and install fonts

---
+++ src/pages/_document.tsx
@@ -0,0 +1,23 @@
+// _document.tsx src/pages/_document.tsx
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang='en'>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&family=Roboto+Slab:wght@400;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
