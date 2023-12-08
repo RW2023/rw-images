4a22701a79ebfca3aff2ac842933d3369c5a1d5a
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-08 16:24:48 -0500

    update heading text in navbar to my name

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -25,7 +25,7 @@ const Navbar = () => {
             className="font-semibold text-xl tracking-tight cursor-pointer"
           >
             <div>
-              <Heading title="r w images" />
+              <Heading title="ryan wilson" iconClass="fas fa-camera" />
             </div>
           </span>
         </Link>
