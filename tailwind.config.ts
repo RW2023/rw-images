0f60b7de1f9297fd3d7c746771367acbead90b16
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-08 13:54:41 -0500

    install and configure daisy ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,34 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  daisyui: {
+    themes: true,
+  },
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        overlay: 'rgba(43, 45, 66, 0.5)',
+        background: '#2b2d42',
+        button: '#f8f32b',
+        headline: '#ffffff',
+        paragraph: '#8d99ae',
+        buttonText: '#2b2d42',
+        stroke: '#8d99ae',
+        main: '#f8f32b',
+        highlight: '#f8f32b',
+        secondary: '#8d99ae',
+        tertiary: '#000000',
+      },
+      fontFamily: {
+        sans: ['Poppins', 'sans'],
+        serif: ['Roboto Slab', 'serif'],
       },
     },
   },
-  plugins: [],
-}
-export default config
+  plugins: [require('daisyui')],
+};
