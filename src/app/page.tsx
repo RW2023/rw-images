6f92a9116f21d33b215c6aed609835c490e28fd4
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-08 07:34:43 -0500

    import and use heading component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import Heading from '@/components/ui/Heading'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <div>
-      <h1>Home Page</h1>
+      <Heading  title='home page'/>
     </div>
   )
 }
