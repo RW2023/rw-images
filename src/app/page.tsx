b0a7bd9fa2209ccd44e970e5ded8d908d8762561
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-08 16:24:09 -0500

    set temp home dive to screen hight

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div>
+    <div className='container flex flex-col flex-grow h-screen'>
       <Heading  title='home page'/>
     </div>
   )
