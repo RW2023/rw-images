7e190cea79305ecb26dc492c74c90721333b93f4
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-09 06:01:20 -0500

    remove h-screen

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -30,7 +30,7 @@ export default function Gallery() {
   }, []);
 
   return (
-    <div className="container mx-auto p-4 h-screen">
+    <div className="container mx-auto p-4">
       <Heading title="Gallery" />
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
         {images.map((img, index) => (
