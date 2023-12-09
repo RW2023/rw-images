625739f2f43790614456df7dc92537d1c87f7a60
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-08 23:56:23 -0500

    add home icon to heading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
+//src/app/page.tsx
 import Heading from '@/components/ui/Heading'
-import Image from 'next/image'
 
 export default function Home() {
   return (
     <div className='container flex flex-col flex-grow h-screen'>
-      <Heading  title='home page'/>
+      <Heading  title='home page' iconClass='fas fa-home'/>
     </div>
   )
 }
