7938e7ae72d9a5e2459ff49e3e82dc9f9cf8edc8
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-09 11:39:07 -0500

    update heading

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -60,7 +60,7 @@ export default function Gallery() {
 
   return (
     <div className="container mx-auto p-4">
-      <Heading title="Gallery" />
+      <Heading title="Gallery" iconClass='fas fa-images' />
       <div
         ref={galleryRef}
         className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4"
