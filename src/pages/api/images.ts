0e71a6eaa485479f9ae82cf90adaec3a0ecac3f4
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-10 01:19:28 -0500

    impliment caching for images to reduce api calls and improve performance

--- src/pages/api/images.ts
+++ src/pages/api/images.ts
@@ -1,19 +1,14 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { v2 as cloudinary } from 'cloudinary';
 
-// Define a type for the response from Cloudinary
 type CloudinaryImage = {
   secure_url: string;
-  // Include other properties from Cloudinary's response as needed
 };
 
-// Define the image type for your application
 type Image = {
   url: string;
-  // Add any other relevant properties
 };
 
-// Cloudinary configuration
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.API_KEY,
@@ -26,16 +21,20 @@ export default async function handler(
 ) {
   try {
     const { resources } = await cloudinary.search
-      .expression('folder:Photography/portfolio') 
+      .expression('folder:Photography/portfolio')
       .sort_by('public_id', 'desc')
       .max_results(30)
       .execute();
 
     const images = resources.map((file: CloudinaryImage) => ({
       url: file.secure_url,
-      // Map other properties if needed
     }));
 
+    // Set cache headers
+    res.setHeader(
+      'Cache-Control',
+      'public, s-maxage=3600, stale-while-revalidate=600',
+    );
     res.status(200).json(images);
   } catch (error) {
     console.error('Error fetching images:', error);
