46aae98a31d12583ce0a11e9a6a50cced6a91238
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-10 01:07:20 -0500

    update theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" data-theme="dim">
+    <html lang="en" data-theme="business">
       <head>
         <link
           rel="stylesheet"
