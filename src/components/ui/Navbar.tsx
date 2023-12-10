f3c007d6fd8f4fcc65837a9a63ebb0cf91450266
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-10 16:41:03 -0500

    change navbar icon

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -25,7 +25,7 @@ const Navbar = () => {
             className="font-semibold text-xl tracking-tight cursor-pointer"
           >
             <div>
-              <Heading title="r w" iconClass="fas fa-camera" />
+              <Heading title="r w" iconClass="fas fa-camera-retro" />
             </div>
           </span>
         </Link>
