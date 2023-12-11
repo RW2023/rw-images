9fbaf0a1549d2137dcab90fb68b30bdcd16bc648
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-10 22:18:29 -0500

    create ai page duplicate image gallery page and add logic to fetch ai images

---
+++ src/app/ai/page.tsx
@@ -0,0 +1,50 @@
+'use client';
+import React, { useState, useEffect } from 'react';
+import Heading from '@/components/ui/Heading';
+import Loading from '@/components/ui/Loading';
+import ImageGallery from 'react-image-gallery';
+import 'react-image-gallery/styles/css/image-gallery.css';
+
+interface ImageData {
+  url: string;
+}
+
+export default function Gallery() {
+  const [images, setImages] = useState<ImageData[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchImages() {
+      try {
+        const response = await fetch('/api/ai/');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data: ImageData[] = await response.json();
+        setImages(data);
+      } catch (error) {
+        console.error('Fetch error:', error);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchImages();
+  }, []);
+
+  const galleryImages = images.map((img) => ({
+    original: `${img.url}?q_auto,f_auto,w_900,h_600,c_fill`, // Optimized for larger view
+    thumbnail: `${img.url}?q_auto,f_auto,w_100,h_100,c_fill`, // Optimized thumbnails
+  }));
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  return (
+    <div className="container mx-auto p-4">
+      <Heading title="Gallery" iconClass="fas fa-images" />
+      <ImageGallery items={galleryImages} thumbnailPosition="bottom" />
+    </div>
+  );
+}
