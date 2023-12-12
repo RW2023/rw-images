c93a63ee7f0c80372c9dc4329023e5a34197c2d8
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-12 14:23:50 -0500

    Refactor image gallery to use Next.js Image and MUI Masonry

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,9 +1,11 @@
+// src/app/gallery/page.tsx
 'use client';
 import React, { useState, useEffect } from 'react';
 import Heading from '@/components/ui/Heading';
 import Loading from '@/components/ui/Loading';
-import ImageGallery from 'react-image-gallery';
-import 'react-image-gallery/styles/css/image-gallery.css';
+import Image from 'next/image';
+import Masonry from '@mui/lab/Masonry';
+import '@mui/lab/Masonry';
 
 interface ImageData {
   url: string;
@@ -32,19 +34,27 @@ export default function Gallery() {
     fetchImages();
   }, []);
 
-  const galleryImages = images.map((img) => ({
-    original: `${img.url}?q_auto:good,f_auto,c_limit,w_auto,dpr_auto`, // Optimized for larger view
-    thumbnail: `${img.url}?q_auto:good,f_auto,c_limit,w_100,dpr_auto`, // Optimized thumbnails
-  }));
-
   if (isLoading) {
     return <Loading />;
   }
 
   return (
-    <div className="container mx-auto p-4">
+    <div className="container mx-auto p-4 justify-between">
       <Heading title="Photography" iconClass="fas fa-images" />
-      <ImageGallery items={galleryImages} thumbnailPosition="left" />
+      <Masonry columns={3} spacing={2} className="my-4">
+        {images.map((img, index) => (
+          <div key={index} className="image-item">
+            <Image
+              src={`${img.url}?q_auto:good,f_auto,c_limit,w_auto,dpr_auto`}
+              alt="Photography Image"
+              width={300}
+              height={300}
+              layout="responsive"
+              className="rounded border border-stroke-500 bg-black drop-shadow-md m-1 p-1"
+            />
+          </div>
+        ))}
+      </Masonry>
     </div>
   );
 }
