e2532288c1abbb2f9fafe5389db2ce4abec515f8
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-12 20:10:35 -0500

    Add lightbox functionality to image gallery

--- src/app/ai/page.tsx
+++ src/app/ai/page.tsx
@@ -1,11 +1,12 @@
+/* eslint-disable @next/next/no-img-element */
 // src/app/ai/page.tsx
 'use client';
 import React, { useState, useEffect } from 'react';
 import Heading from '@/components/ui/Heading';
 import Loading from '@/components/ui/Loading';
-import Image from 'next/image';
+import Lightbox from 'yet-another-react-lightbox';
+import 'yet-another-react-lightbox/styles.css';
 import Masonry from '@mui/lab/Masonry';
-import '@mui/lab/Masonry';
 
 interface ImageData {
   url: string;
@@ -14,6 +15,8 @@ interface ImageData {
 export default function Gallery() {
   const [images, setImages] = useState<ImageData[]>([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [lightboxOpen, setLightboxOpen] = useState(false);
+  const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
 
   useEffect(() => {
     async function fetchImages() {
@@ -34,6 +37,11 @@ export default function Gallery() {
     fetchImages();
   }, []);
 
+  const handleImageClick = (index: number) => {
+    setCurrentSlideIndex(index);
+    setLightboxOpen(true);
+  };
+
   if (isLoading) {
     return <Loading />;
   }
@@ -43,18 +51,26 @@ export default function Gallery() {
       <Heading title="AI Generated" iconClass="fas fa-robot" />
       <Masonry columns={3} spacing={1} className="my-4 justify-between">
         {images.map((img, index) => (
-          <div key={index} className="image-item">
-            <Image
+          <div
+            key={index}
+            className="image-item"
+            onClick={() => handleImageClick(index)}
+          >
+            <img
               src={`${img.url}?q_auto:good,f_auto,c_limit,w_auto,dpr_auto`}
               alt="AI Generated Image"
-              width={300}
-              height={300}
-              layout="responsive"
               className="rounded border border-stroke-500 bg-black drop-shadow-md m-1 p-1"
+              style={{ width: '100%', height: 'auto' }}
             />
           </div>
         ))}
       </Masonry>
+      <Lightbox
+        open={lightboxOpen}
+        close={() => setLightboxOpen(false)}
+        slides={images.map((img) => ({ src: img.url }))}
+        index={currentSlideIndex}
+      />
     </div>
   );
 }
