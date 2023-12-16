e617bef6fbc781f6bea73cda23a4be15c1dc7510
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-15 20:06:55 -0500

    Add dark mode colors to tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,9 +23,16 @@ module.exports = {
         highlight: '#f8f32b',
         secondary: '#8d99ae',
         tertiary: '#000000',
+        // Dark mode specific colors
+        dark: {
+          headline: '#ffffff', // White for headlines
+          paragraph: '#c0c7d0', // Lighter grey for paragraphs
+          buttonText: '#d1d5db', // Light grey for button text
+          // Add other dark mode colors as needed
+        },
       },
       fontFamily: {
-        sans: ['Poppins', 'sans'],
+        sans: ['Poppins', 'sans-serif'],
         serif: ['Roboto Slab', 'serif'],
       },
     },
